be40417448b3b37c819faa7beee8660c2461890a
Your Name <tekopc>
2021-10-19 21:12:28 -0300

    probando

--- trabajo-practico/src/views/Logout.vue
+++ trabajo-practico/src/views/Logout.vue
@@ -1,9 +1,20 @@
 <template>
-  
+  <h2>Logout</h2>
+  <form @submit.prevent="logout">
+      <button type="submit">Logout</button>
+
+  </form>
 </template>
 
 <script>
 export default {
+    name : 'Login',
+    methods : {
+        logout(){
+            console.log('Logout');
+            //this.$store.
+        }
+    }
 
 }
 </script>
