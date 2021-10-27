e4402b168891f83d0bcdf7a7cdad443faa50b6ad
Your Name <tekopc>
2021-10-27 20:27:08 -0300

    reordenamiento del proyecto

--- trabajo-practico/Cliente/src/views/ListaProducto.vue
+++ trabajo-practico/Cliente/src/views/ListaProducto.vue
@@ -1,6 +1,6 @@
 <template>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <div>
+  <div id='lista' class="container-sm">
         <table class="table">
     <thead>
         <tr>
@@ -22,12 +22,21 @@
   </div>
 </template>
 
+
+
+
+
 <script>
+
+
+
 export default {
     name : 'ListaProducto'
 }
 </script>
 
 <style>
-
+#lista{
+    padding-top: 45px;
+}
 </style>
