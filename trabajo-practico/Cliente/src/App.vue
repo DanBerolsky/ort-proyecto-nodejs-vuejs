8605b3236d1b1071936b5eed616fba46a487e1e3
guidio30 <82186263+guidio30@users.noreply.github.com>
2021-10-27 21:02:30 -0300

    probando

--- trabajo-practico/Cliente/src/App.vue
+++ trabajo-practico/Cliente/src/App.vue
@@ -1,27 +1,4 @@
 <template>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    
-    <router-link class="navbar-brand" to="/">Home</router-link>
-    <router-link class="navbar-brand" to="/about">About</router-link>
-    <router-link class="navbar-brand" to="/Sistema">Sistema</router-link>
-    <router-link class="navbar-brand" to="/ListaProducto">ListaProductos</router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
-        
-      </div>
-    </div>
-  </div>
-</nav>
-  
-  <router-view/>
+ 
+ 
 </template>
--- trabajo-practico/Cliente/src/main.js
+++ trabajo-practico/Cliente/src/main.js
@@ -1,10 +1,17 @@
+import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {store} from './store'
-import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 createApp(App).use(router).use(router).use(store).use(router).mount('#app')
 
