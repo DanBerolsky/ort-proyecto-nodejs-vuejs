35588663fbf98da6538e752e95154b446fb5e628
guidio30 <82186263+guidio30@users.noreply.github.com>
2021-10-29 08:02:17 -0300

    Update main.js

--- trabajo-practico/Cliente/src/main.js
+++ trabajo-practico/Cliente/src/main.js
@@ -3,15 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {store} from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 Vue.config.productionTip = false
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 createApp(App).use(router).use(router).use(store).use(router).mount('#app')
 
